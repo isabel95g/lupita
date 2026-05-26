3596ed264f1454f2073d5dd5fa17ef07b5dec31d
IsabelGomez95 <211209012@acatlan.tecnm.mx>
2026-05-26 16:08:53 -0600

    dg

--- js/admin.js
+++
@@ -1,7 +0,0 @@
-// js/admin.js
-function cerrarSesion() {
-    if (confirm('¿Estás seguro de cerrar sesión?')) {
-        sessionStorage.removeItem('usuario');
-        window.location.href = '/';
-    }
-}
